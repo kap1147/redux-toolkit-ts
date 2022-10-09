c89451611f91059032c49e277acdb045cadb0e61
kap1147 <joshua1147.ad@gmail.com>
2022-10-09 14:10:00 -0400

    removed hooks dir

--- src/hooks/useAppDispatch.tsx
+++
@@ -1,5 +0,0 @@
-import { store } from '../app/store';
-import { useDispatch } from 'react-redux';
-
-export type AppDispatch = typeof store.dispatch
-export const useAppDispatch: () => AppDispatch = useDispatch
